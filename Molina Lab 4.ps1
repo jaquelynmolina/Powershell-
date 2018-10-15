01909b49c254ceacda3c738f3dadac965817ffc5
JaquelynMolina <jaquelynmolina@manhattantech.edu>
2018-10-15 16:38:12 -0500

    Update Molina Lab 4.ps1

---
+++ Molina Lab 4.ps1
@@ -0,0 +1,10 @@
+﻿new-item -type directory -name fileserver -path C:\ 
+$csv = Import-Csv C:\employeelist.csv
+
+foreach ($name in $csv){
+    new-item -type directory -path C:\fileserver -name $name.Name.replace(' ', '').tolower()
+    }
+
+foreach ($thing in get-childitem C:\fileserver){
+    new-item -name hello.txt -value "welcome message" -path C:\fileserver\$thing
+    }
