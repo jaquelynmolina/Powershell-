0cf0e1876bbdef02908cb2854b9bd86796bc5f1c
JaquelynMolina <jaquelynmolina@manhattantech.edu>
2018-10-18 11:38:35 -0500

    Molina Quiz 5
    Quiz 5

---
+++ Molina Quiz 5.ps1
@@ -0,0 +1,31 @@
+﻿#ask a user to input a number, and using if statements, 
+#tell the user if it is higher or lower than a number you pick out
+
+[int]$numberpick = read-host "pick a number"
+if($numberpick -le 19){
+    write-host "Your number is smaller than mine"
+    }
+if($numberpick -ge 21){
+    write-host "Your number is larger than mine."
+    }
+
+#Using Get-Process get only the StartTime property and store it in a variable
+
+$starttime = get-process | select-object starttime
+
+#Using a loop print each line of the variable you stored the start times in, 
+#and have it print the start time each time the loop runs and a message 
+#telling the user what it is outputting.
+
+
+foreach($time in $starttime){
+    $processname = get-process | select-object processname
+    }
+foreach($name in $processname){
+    $processname = get-process | select-object processname
+    write-host "$time is the start time of $name."
+    }
+
+#Using git make a local copy of this repository https://github.com/PowerShell/PowerShell
+
+git clone https://github.com/PowerShell/PowerShell
