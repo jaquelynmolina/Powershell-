ff3ac58f8d9306720629a8aab968a04545274d80
jaquelynmolina <43999657+jaquelynmolina@users.noreply.github.com>
2018-11-27 12:37:15 -0600

    Lab 7

---
+++ Molina Lab 7.ps1
@@ -0,0 +1,18 @@
+﻿#put the whole script in a while loop that asks the user if they want to exit
+
+$no = read-host "Do you want to exit? (y/n)" 
+while($no -eq "n"){
+    
+
+$serverlist = import-csv C:\PowershellWork\serverlist.csv
+
+
+foreach($server in $serverlist.servers){
+    Start-Job -inputobject $server -ScriptBlock{test-connection -ComputerName $input}
+    }
+
+get-job | receive-job
+
+$no = read-host "Do you want to exit? (y/n)"
+
+}
